5798ff3a5480393e778daac03ca71b56dbb0b7bb
imimali <imimali18@gmail.com>
2020-01-04 18:48:21 +0200

    add synchronized dictionary

--- tables/tables.py
+++ tables/tables.py
@@ -4,6 +4,7 @@
     @author: Gergely
 '''
 from collections import namedtuple
+from threading import Lock
 
 LockTableEntry = namedtuple('LockTableEntry',
                             'id '
@@ -11,13 +12,29 @@ LockTableEntry = namedtuple('LockTableEntry',
                             'record_id '
                             'table '
                             'transaction')
+
 TransactionTableEntry = namedtuple('TransactionTableEntry',
                                    'id '
                                    'timestamp '
                                    'status')
+
 WaitForGraphEntry = namedtuple('WaitForGraphEntry',
                                'lock_type '
                                'locked_table '
                                'locked_object '
                                'trans_has_lock '
                                'trans_waits_lock')
+
+
+class SynchronizedTable:
+    def __init__(self):
+        self.elems = {}
+        self.lock = Lock()
+
+    def __getitem__(self, item):
+        with self.lock:
+            return self.elems[item]
+
+    def __setitem__(self, key, value):
+        with self.lock:
+            self.elems[key] = value
