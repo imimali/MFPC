210e38db449242d1dd3daff8d573842df1d0131f
imimali <imimali18@gmail.com>
2020-01-24 23:17:00 +0200

    refactor ui

--- ui/ui.py
+++ ui/ui.py
@@ -3,12 +3,14 @@
 
     @author: Gergely
 '''
+import os
 import sys
 
-from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtCore import pyqtSlot, QRect
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import *
+
+from ui.widgets import MTableWidget, MFormWidget, TransactionUtilsWidget
 
 font = QFont('Verdana', 13)
 font2 = QFont('Comic Sans MS', 12)
@@ -28,95 +30,32 @@ class App(QWidget):
     def init_ui(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.setFont(font)
+        self.setFont(font2)
         main_layout = QVBoxLayout()
 
         # transaction
-        run_transaction_layout = QVBoxLayout()
-        run_transaction_layout.setContentsMargins(200, 0, 200, 0)
-        transaction_list = QListWidget()
-        run_transaction_layout.addWidget(transaction_list)
-
-        manage_buttons_layout = QHBoxLayout()
-        enough_button = QPushButton('Enough')
-        start_new_transaction_button = QPushButton('New Transaction')
-        manage_buttons_layout.addWidget(start_new_transaction_button)
-        manage_buttons_layout.addWidget(enough_button)
-        run_transaction_button = QPushButton('Run Transaction')
-        run_transaction_layout.addLayout(manage_buttons_layout)
-        run_transaction_layout.addWidget(run_transaction_button)
-        run_transaction_button.setFont(font)
 
         edits_layout = QHBoxLayout()
         edits_layout.setSpacing(24)
-        edits_layout.addLayout(self.build_form(title='Person', row_names=['id', 'name', 'age', 'email']))
-        edits_layout.addLayout(self.build_form(title='Rental', row_names=['id', 'person_id', 'movie_id']))
-        edits_layout.addLayout(self.build_form(title='Rental', row_names=['id', 'year', 'title', 'rating']))
+        person_form = MFormWidget(title='Person', row_names=['id', 'name', 'age', 'email'])
+        rental_form = MFormWidget(title='Rental', row_names=['id', 'person_id', 'movie_id'])
+        movie_form = MFormWidget(title='Rental', row_names=['id', 'year', 'title', 'rating'])
+        for widget in [person_form, rental_form, movie_form]:
+            edits_layout.addWidget(widget)
 
         tables_layout = QHBoxLayout()
         tables_layout.setSpacing(16)
-        tables_layout.addLayout(App.build_table(['id', 'name', 'age', 'email'], title='Person'))
-        tables_layout.addLayout(App.build_table(['id', 'person_id', 'movie_id'], title='Rental'))
-        tables_layout.addLayout(App.build_table(['id', 'year', 'title', 'rating'], title='Movie'))
+        person_table = MTableWidget(['id', 'name', 'age', 'email'], title='Person')
+        rental_table = MTableWidget(['id', 'person_id', 'movie_id'], title='Rental')
+        movie_table = MTableWidget(['id', 'year', 'title', 'rating'], title='Movie')
+        for widget in [person_table, rental_table, movie_table]:
+            tables_layout.addWidget(widget)
 
         main_layout.addLayout(tables_layout)
-        main_layout.addLayout(run_transaction_layout)
+        main_layout.addWidget(TransactionUtilsWidget())
         main_layout.addLayout(edits_layout)
         self.setLayout(main_layout)
         self.show()
-
-    @staticmethod
-    def build_form(title, row_names):
-        edit_layout = QFormLayout()
-        edit_layout.setSpacing(4)
-        edit_layout.addWidget(QLabel(title))
-        for row in row_names:
-            edit_layout.addRow(row, App.build_form_row())
-        edit_layout.addRow(App.build_button_bar())
-        return edit_layout
-
-    @staticmethod
-    def build_button_bar():
-        button_bar = QHBoxLayout()
-        for button_name in ['Add', 'Update', 'Delete']:
-            button = QPushButton(button_name)
-            button.setFixedWidth(100)
-            button.setFixedHeight(40)
-            button.setContentsMargins(20, 0, 20, 0)
-            button_bar.addWidget(button)
-        return button_bar
-
-    @staticmethod
-    def build_form_row():
-        value_edit = QTextEdit()
-        value_edit.setFixedHeight(40)
-        value_edit.setFixedWidth(120)
-        return value_edit
-
-    @staticmethod
-    def build_table(columns_names, rows=0, title=''):
-        table = QTableWidget()
-        table.setContentsMargins(10, 10, 10, 10)
-        table.setColumnCount(len(columns_names))
-        table.setRowCount(3)
-        table.setHorizontalHeaderLabels(columns_names)
-        table.resizeColumnsToContents()
-        table.resizeRowsToContents()
-        table.setSelectionBehavior(QAbstractItemView.SelectRows)
-        for i in range(rows):
-            for j in range(len(columns_names)):
-                # item = QTableWidgetItem('asd')
-                table.setItem(i, j, QTableWidgetItem(f'iiiiiiiiiiiiiiiii={i};j={j}'))
-
-        wrapper = QVBoxLayout()
-
-        wrapper.addWidget(QLabel(title))
-        wrapper.addWidget(table)
-        fill_button = QPushButton('Fill')
-        fill_button.setFixedWidth(100)
-        wrapper.addWidget(fill_button)
-
-        return wrapper
 
 
 if __name__ == '__main__':
---
+++ ui/widgets.py
@@ -0,0 +1,101 @@
+'''
+    created on 24 January 2020
+    
+    @author: Gergely
+'''
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QPushButton, QTableWidget, QAbstractItemView, QVBoxLayout, QLabel, \
+    QFormLayout, QLineEdit, QListWidget
+
+
+class TransactionUtilsWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        run_transaction_layout = QVBoxLayout()
+        run_transaction_layout.setContentsMargins(200, 0, 200, 0)
+        transaction_list = QListWidget()
+        run_transaction_layout.addWidget(transaction_list)
+
+        manage_buttons_layout = QHBoxLayout()
+        enough_button = QPushButton('Enough')
+        start_new_transaction_button = QPushButton('New Transaction')
+        manage_buttons_layout.addWidget(start_new_transaction_button)
+        manage_buttons_layout.addWidget(enough_button)
+        run_transaction_button = QPushButton('Run Transaction')
+        run_transaction_layout.addLayout(manage_buttons_layout)
+        run_transaction_layout.addWidget(run_transaction_button)
+        run_transaction_button.setFont(QFont('Verdana', 13))
+
+        self.transaction_list = transaction_list
+        self.start_new_transaction_button = start_new_transaction_button
+        self.enough_button = enough_button
+        self.run_button = run_transaction_button
+        self.setLayout(run_transaction_layout)
+
+    def connect(self):
+        pass
+
+
+class MFormWidget(QWidget):
+    def __init__(self, title, row_names):
+        super().__init__()
+        edit_layout = QFormLayout()
+        edit_layout.setSpacing(4)
+        edit_layout.addWidget(QLabel(title))
+        for row in row_names:
+            value_edit = QLineEdit()
+            value_edit.setFixedHeight(40)
+            value_edit.setFixedWidth(120)
+            edit_layout.addRow(row, value_edit)
+        edit_layout.addRow(AUDButtonBar())
+        self.setLayout(edit_layout)
+
+
+class AUDButtonBar(QWidget):
+    def __init__(self):
+        super().__init__()
+        button_bar = QHBoxLayout()
+        self.buttons = {}
+        for button_name in ['Add', 'Update', 'Delete']:
+            button = QPushButton(button_name)
+            button.setFixedWidth(100)
+            button.setFixedHeight(40)
+            button.setContentsMargins(20, 0, 20, 0)
+            button_bar.addWidget(button)
+            self.buttons[button_name] = button
+        self.setLayout(button_bar)
+
+    def connect_buttons(self, callback_map):
+        for button_name in self.buttons:
+            self.buttons[button_name].clicked.connect(callback_map[button_name])
+
+
+class MTableWidget(QWidget):
+    def __init__(self, columns_names, title):
+        super().__init__()
+        table = QTableWidget()
+        table.setContentsMargins(10, 10, 10, 10)
+        table.setColumnCount(len(columns_names))
+        table.setRowCount(3)
+        table.setHorizontalHeaderLabels(columns_names)
+        table.resizeColumnsToContents()
+        table.resizeRowsToContents()
+        table.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        wrapper = QVBoxLayout()
+
+        wrapper.addWidget(QLabel(title))
+        wrapper.addWidget(table)
+        fill_button = QPushButton('Fill')
+        fill_button.setFixedWidth(100)
+        wrapper.addWidget(fill_button)
+
+        self.table = table
+        self.fill_button = fill_button
+        self.setLayout(wrapper)
+
+    def connect_fill_button(self, connect_callback):
+        self.fill_button.clicked.connect(connect_callback)
+
+    def fill_table(self):
+        pass
