2173aa628301d315c97b8e5c7f0577f3b8554cd8
imimali <imimali18@gmail.com>
2020-02-03 12:44:50 +0200

    add main

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+    created on 02 February 2020
+    
+    @author: Gergely
+'''
+import sys
+from dao.transaction import Transaction
+from PyQt5.QtWidgets import QApplication
+
+from ui.ui import App
+
+# TODO use other tables
+# TODO specify database name at ui
+# TODO actually fill tables
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    Transaction.launch_deadlock_checker_daemon()
+    ex = App()
+    sys.exit(app.exec_())
